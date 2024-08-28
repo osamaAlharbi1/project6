f04817db2a3d5efca27073b86723c90a8491a693
osamaAlharbi1 <sdfd567uu@gmail.com>
2024-08-28 14:51:29 +0300

    add files

--- main.py
+++ main.py
@@ -1,14 +1,59 @@
-from typing import Optional
-
 from fastapi import FastAPI
+import joblib
+from pydantic import BaseModel
+
 
 app = FastAPI()
-
-
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return "Welcome To Tuwaiq Academy"
+
+# get request
+@app.get("/items/")
+def create_item(item: dict):
+    return {"item": item}
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+model = joblib.load('knn_model.joblib')
+scaler = joblib.load('scaler.joblib')
+
+
+
+# Define a Pydantic model for input data validation
+class InputFeatures(BaseModel):
+    Year: int
+    Engine_Size: float
+    Mileage: float
+    Type: str
+    Make: str
+    Options: str
+
+def preprocessing(input_features: InputFeatures):
+    dict_f = {
+    'Year': input_features.Year,
+    'Engine_Size': input_features.Engine_Size,
+    'Mileage': input_features.Mileage,
+    'Type_Accent': input_features.Type == 'Accent',
+    'Type_Land Cruiser': input_features.Type == 'Land Cruiser',
+    'Make_Hyundai': input_features.Make == 'Hyundai',
+    'Make_Mercedes': input_features.Make == 'Mercedes',
+    'Options_Full': input_features.Options == 'Full',
+    'Options_Standard': input_features.Options == 'Standard'
+    }
+    # Convert dictionary values to a list in the correct order
+    features_list = [dict_f[key] for key in sorted(dict_f)]
+    # Scale the input features
+    scaled_features = scaler.transform([list(dict_f.values
+    ())])
+    return scaled_features    
+
+@app.post("/predict")
+async def predict(input_features: InputFeatures):
+    data = preprocessing(input_features)
+    y_pred = model.predict(data)
+    print(y_pred)
+    return {"pred": y_pred.tolist()[0]}
+
